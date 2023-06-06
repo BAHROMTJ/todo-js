cb4f1dbda97fb70731d95dbec3ccebe3d8ab1666
BAHROMTJ <sarifzodabahromofficall@gmail.com>
2023-06-06 09:17:12 +0500

    JGJHJ

--- js/main.js
+++ js/main.js
@@ -1,109 +1,138 @@
-let dialog=document.querySelector(".dialog")
-let dialog2=document.querySelector(".dialog2")
-let add=document.querySelector(".add")
-let form=document.querySelector(".form")
-let contener=document.querySelector(".contener")
-let arr=[]
-let close=document.querySelector(".dclose")
+let url = "https://6264fc9e94374a2c506bde51.mockapi.io/";
 
+let dialog = document.querySelector(".dialog");
+let dialog2 = document.querySelector(".dialog2");
+let add = document.querySelector(".add");
+let form = document.querySelector(".form");
+let contener = document.querySelector(".contener");
+let arr = [];
+let close = document.querySelector(".dclose");
+let form1 = document.querySelector(".form1");
 
-close.onclick=()=>{
-    dialog.close();   
+close.onclick = () => {
+  dialog.close();
+};
+
+add.onclick = () => {
+  dialog.show();
+};
+
+async function postfn(obj) {
+  try {
+ await axios.post(`${url}todos`, obj);
+    getfn();
+  } catch (error) {}
 }
 
-add.onclick=()=>{
-    dialog.show();
-   
+async function getfn() {
+  try {
+    let { data } = await axios.get(`${url}todos`);
+
+    arr = [...data];
+
+    render();
+  } catch (error) {}
 }
 
-form.onsubmit=(Event)=>{
-  
-    Event.preventDefault();
-    let target=Event.target
-   
-    let obj={
-    id:new Date().getTime(),
-  name:target.dtxt.value,
-comp:false,
-    }
-    arr.push(obj)
-render()
-dialog.close();
+form.onsubmit = (Event) => {
+  Event.preventDefault();
+  let target = Event.target;
+
+  let obj = {
+    title: target.dtxt.value,
+    comp: false,
+  };
+  postfn(obj);
+  dialog.close();
+};
+
+
+// DELIT
+async function dellit(id) {
+  try {
+    let { data } = await axios.delete(`${url}todos/${id}`);
+    getfn();
+  } catch (error) {}
+}
+
+// edit
+async function edit(elem, id) {
+  console.log(elem);
+  dialog2.show();
+  form1.dtxt.value = elem.title;
+
+  form1.ok.onclick = async () => {
+    alert("инро " + elem.title + " ба ин " + form1.dtxt.value + " иваз мекуни");
+    elem.title = form1.dtxt.value;
+    try {
+     await axios.put(`${url}todos/${id}`, elem);
+    } catch (error) {}
+
+    dialog2.close();
+    getfn();
+  };
+}
+
+// chek
+async function chekin(chek, id) {
+  let todo = arr.find((elem) => elem.id == id);
+  todo.comp = chek;
+
+  try {
+    await axios.put(`${url}todos/${id}`, todo);
+    render();
+  } catch (error) {}
 }
 
 function render() {
-    let views=document.querySelector(".views")
- views.innerHTML=""
+  let views = document.querySelector(".views");
+  views.innerHTML = "";
 
-arr.forEach(elem => {
-let chek=document.createElement("input")
-let div=document.createElement("div")
-let span=document.createElement("span")
-let btn=document.createElement("button")
-let del=document.createElement("button")
-let spandel=document.createElement("span")
-chek.type="checkbox"
-del.innerText="Del"
-div.classList.add('view')
-btn.classList.add("edit")
-span.classList.add('span')
-del.classList.add('del')
-span.innerHTML=elem.name
-btn.innerHTML='Edit'
-div.appendChild(chek)
-views.appendChild(div)
-div.appendChild(span)
-div.appendChild(spandel)
+  arr.forEach((elem) => {
+    let chek = document.createElement("input");
+    let div = document.createElement("div");
+    let span = document.createElement("span");
+    let btn = document.createElement("button");
+    let del = document.createElement("button");
+    let spandel = document.createElement("span");
+    chek.type = "checkbox";
+    del.innerText = "Del";
+    div.classList.add("view");
+    btn.classList.add("edit");
+    span.classList.add("span");
+    del.classList.add("del");
+    span.innerHTML = elem.title;
+    btn.innerHTML = "Edit";
+    div.appendChild(chek);
+    views.appendChild(div);
+    div.appendChild(span);
+    div.appendChild(spandel);
 
-spandel.appendChild(del)
-spandel.appendChild(btn)
+    spandel.appendChild(del);
+    spandel.appendChild(btn);
 
-chek.checked=elem.comp
+    chek.checked = elem.comp;
 
-  chek.onclick=()=>{
+    chek.onchange = () => {
+      chekin(chek.checked, elem.id);
+    };
+
     if (chek.checked) {
-span.classList.add('chek')
- 
-	}
-	else {
-    span.classList.remove('chek')
+      span.classList.add("chek");
+    }
+    btn.onclick = () => {
+      edit(elem, elem.id);
+    };
 
-	}
-  }
+    form1.close.onclick = () => {
+      dialog2.close();
+    };
 
-btn.onclick=()=>{
-    let form1=document.querySelector(".form1")
-
-    dialog2.show()
-  form1.dtxt.value=elem.name
-form1.ok.onclick=()=>{
-   alert( "инро "+elem.name+" ба ин "+ form1.dtxt.value+' иваз мекуни')
-   elem.name=form1.dtxt.value
-   
-    dialog2.close()
- render() 
-   
+    del.onclick = () => {
+      alert(" АНИК УДАЛИТ МЕКУНЕД ХАМИНРО-- " + elem.title);
+      dellit(elem.id);
+    };
+  });
 }
 
-form1.close.onclick=()=>{
-  dialog2.close()
- }
- 
-
-}
-
-del.onclick=()=>{
-    alert(' АНИК УДАЛИТ МЕКУНЕД ХАМИНРО-- '+elem.name)
-    del(elem.id)
-   function del (id) {
-    arr=arr.filter(elem=>elem.id!==id)
-    render()
-   }
-
-}
-// let view=document.querySelector(".view")
-// view.innerHTML=""
-
-});
-
-}
+getfn();
